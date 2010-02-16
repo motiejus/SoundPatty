01a3e0dcd71f1460834d4eaabf76f34c9115e88d
Motiejus Jakštys <desired.mta@gmail.com>
2010-02-16 21:14:25 +0200

    Improved validation

--- readit.cpp
+++ readit.cpp
@@ -6,6 +6,9 @@ using namespace std;
 bool check_sample(const char*, const char*);
 void fatal (const char* );
 
+char errmsg[200]; // Temporary message
+int SAMPLERATE;
+
 int main (int argc, char *argv[]) {
     if (argc != 2) {
         fatal ("Unspecified input WAV file. exiting\n");
@@ -13,18 +16,39 @@ int main (int argc, char *argv[]) {
     FILE *in = fopen(argv[1], "rb");
     
     // Read bytes [0-3] and [8-11], they should be "RIFF" and "WAVE" in ASCII
-    char h[5];
-    fread(h, 1, 4, in);
+    char header[5];
+    fread(header, 1, 4, in);
 
     char sample[] = "RIFF";
-    if (! check_sample(sample, h) ) {
+    if (! check_sample(sample, header) ) {
         fatal ("RIFF header not found, exiting\n");
     }
-    long pos = ftell(in);
+	// Checking for compression code (21'st byte is 01, 22'nd - 00, little-endian notation
+	short int tmp[2]; // two-byte integer
+	fseek(in, 0x14, 0); // offset = 20 bytes
+    fread(&tmp, 2, 2, in); // Reading two two-byte samples (comp. code and no. of channels)
+    if ( tmp[0] != 1 ) {
+		sprintf(errmsg, "Only PCM(1) supported, compression code given: %d\n", tmp[0]);
+		fatal (errmsg);
+    }
+	// Number of channels must be "MONO"
+    if ( tmp[1] != 1 ) {
+		sprintf(errmsg, "Only MONO supported, given number of channels: %d\n", tmp[1]);
+		fatal (errmsg);
+    }
+	fread(&SAMPLERATE, 2, 1, in); // Single two-byte sample
+	//printf ("Sample rate: %d\n", SAMPLERATE);
+	fseek(in, 0x24, 0);
+	fread(header, 1, 4, in);
+	strcpy(sample, "data");
+	if (! check_sample(sample, header)) {
+		fatal ("data chunk not found in byte offset=36, bad file.");
+	}
+
+	exit(0);
  
     short int buf [100]; // 200 byte array
     while (! feof(in) ) {
-        printf ("OK\n");
         fread(buf, 2, 100, in);
         for (int i = 0; i < 100; i++) {
             printf ("%d ", buf[i]);
