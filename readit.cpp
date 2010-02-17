8cdfba0c000490f8576998d65e5b21369576fa6c
Motiejus Jakštys <desired.mta@gmail.com>
2010-02-17 21:11:01 +0200

    Now we have some REAL pause locations

--- readit.cpp
+++ readit.cpp
@@ -1,36 +1,82 @@
+/*
+ * Only 16 bit PCM MONO supported now.
+ */
+#include <algorithm>
 #include <stdlib.h>
 #include <string.h>
 #include <cstdio>
+#include <map>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
 #define BUFFERSIZE 1 // Number of sample rates
+#define MINWAVEFREQ 40 // Minimum frequency (assume <40Hz do not exist here)
 
 using namespace std;
 bool check_sample(const char*, const char*);
 void process_headers(const char*);
+void read_cfg(const char*);
 void fatal (const char* );
 
 char errmsg[200]; // Temporary message
 FILE *in;
-int SAMPLERATE, DATA_SIZE;
+int SAMPLE_RATE, // 
+	DATA_SIZE,  // Data chunk size in bytes
+	PROC_SAMPLES; // Minimum number of samples to process and be sure two waves included
+
+map<string, string> cfg; // OK, ok... Is it possible to store any value but string (Rrrr...) here?
 
 int main (int argc, char *argv[]) {
     if (argc != 2) {
-        fatal ("Unspecified input WAV file. exiting\n");
+        fatal ("Unspecified config file. exiting\n");
     }
+	read_cfg(argv[1]);
+    process_headers(cfg["filename"].c_str());
+	istringstream i(cfg["treshold1"]);
+	double treshold1;
+	i >> treshold1;
+	int min_silence = (int)(0x7FFF * treshold1); // Below this value we have "silence, pshhh..."
+	printf ("%d, %d\n", min_silence, PROC_SAMPLES);
 
-    process_headers(argv[1]);
+    short int buf [SAMPLE_RATE * BUFFERSIZE]; // Process buffer every second
+	int here = 0, // Assume sample started
+		iscsi = 0; // Number of silence samples
 
-    short int buf [SAMPLERATE * BUFFERSIZE]; // Process buffer every second
     while (! feof(in) ) {
-        fread(buf, 2, SAMPLERATE * BUFFERSIZE, in);
-        for (int i = 0; i < SAMPLERATE * BUFFERSIZE; i++) {
-            printf ("%d ", buf[i]);
+        fread(buf, 2, SAMPLE_RATE * BUFFERSIZE, in);
+        for (int i = 0; i < SAMPLE_RATE * BUFFERSIZE; i++) {
+			int cur = abs(buf[i]);
+
+			if (cur <= min_silence) {
+				iscsi++;
+				//printf ("%d'th element found. %d < %d\n", iscsi, cur, min_silence);
+			} else {
+				if (iscsi >= PROC_SAMPLES) {
+					printf ("Found long silence. Length: %4.4f s, pos: %4.4f - %4.4f\n", (float)iscsi/SAMPLE_RATE, (float)(here-iscsi)/SAMPLE_RATE, (float)here/SAMPLE_RATE);
+				}
+				iscsi = 0;
+			}
+			here++;
+			//printf ("min_silence is %d, but this value is: %d", min_silence, buf[i]);
         }
-        // Let's do something with that numbers
     }
 
     exit(0);
 
+}
+void read_cfg (const char * filename) {
+	ifstream file;
+	file.open(filename);
+	string line;
+	int x;
+	while (! file.eof() ) {
+		getline(file, line);
+		x = line.find(":");
+		if (x == -1) break; // Last line, exit
+		cfg[line.substr(0,x)] = line.substr(x+2);
+	}
 }
 
 void process_headers(const char * infile) {
@@ -57,8 +103,9 @@ void process_headers(const char * infile) {
 		sprintf(errmsg, "Only MONO supported, given number of channels: %d\n", tmp[1]);
 		fatal (errmsg);
     }
-	fread(&SAMPLERATE, 2, 1, in); // Single two-byte sample
-	//printf ("Sample rate: %d\n", SAMPLERATE);
+	fread(&SAMPLE_RATE, 2, 1, in); // Single two-byte sample
+	//printf ("Sample rate: %d\n", SAMPLE_RATE);
+	PROC_SAMPLES = SAMPLE_RATE / MINWAVEFREQ;
 	fseek(in, 0x24, 0);
 	fread(header, 1, 4, in);
 	strcpy(sample, "data");
