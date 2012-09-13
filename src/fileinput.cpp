394062a7136d65944920aca9d955a97fd5a5cbb0
Motiejus Jakštys <desired.mta@gmail.com>
2012-09-13 09:23:43 +0200

    Remove GPL leftovers

--- src/fileinput.cpp
+++ src/fileinput.cpp
@@ -1,21 +1,3 @@
-/*
- *  fileinput.cpp
- *
- *  Copyright (c) 2010 Motiejus Jakštys
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3.
-
- *  This program is distributed in the hope that it will be usefu
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
-
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 #include "fileinput.h"
 #ifdef HAVE_INOTIFY_INIT
 #include <sys/inotify.h>
--- src/jackinput.cpp
+++ src/jackinput.cpp
@@ -1,21 +1,3 @@
-/*
- *  jackinput.cpp
- *
- *  Copyright (c) 2010 Motiejus Jakštys
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3.
-
- *  This program is distributed in the hope that it will be usefu
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
-
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 #include "jackinput.h"
 
 //JackInput *JackInput::jack_inst = NULL;
